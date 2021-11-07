b32e147a6452ff0be303b3cd1bc501293d61d167
rokas <augunasr@gmail.com>
2021-11-07 17:27:17 +0000

    bugs

--- public/members/create.php
+++ public/members/create.php
@@ -1,35 +1,39 @@
 <?php
-$pdo = require_once '../../database.php';
+try {
+  $pdo = require_once '../../database.php';
 
-$statement = $pdo->prepare('SELECT * FROM schools ORDER BY school DESC');
-$statement->execute();
-$schools = $statement->fetchAll(PDO::FETCH_ASSOC);
+  $statement = $pdo->prepare('SELECT * FROM schools ORDER BY school DESC');
+  $statement->execute();
+  $schools = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-$errors = [];
+  $errors = [];
 
-$name = '';
-$email = '';
-$school_id = '';
+  $name = '';
+  $email = '';
+  $school_id = '';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  
-  require_once '../../validate_member.php';
- 
-  if (empty($errors)) {
-    $query = $pdo->query("SELECT id FROM schools WHERE school = '$school_id'");
-    $query->execute();
-    $id = $query->fetchAll(PDO::FETCH_ASSOC);
-
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
+    require_once '../../validate_member.php';
+  
+    if (empty($errors)) {
+      $query = $pdo->query("SELECT id FROM schools WHERE school = '$school_id'");
+      $query->execute();
+      $id = $query->fetchAll(PDO::FETCH_ASSOC);
 
-    $statement = $pdo->prepare('INSERT INTO members (name, email, school_id) VALUES (:name, :email, :school_id)');
+      
 
-    $statement->bindValue(':name', $name);
-    $statement->bindValue(':email', $email);
-    $statement->bindValue(':school_id', (int)$id[0]["id"]);
-    $statement->execute();
-    header('Location: index.php');
+      $statement = $pdo->prepare('INSERT INTO members (name, email, school_id) VALUES (:name, :email, :school_id)');
+
+      $statement->bindValue(':name', $name);
+      $statement->bindValue(':email', $email);
+      $statement->bindValue(':school_id', (int)$id[0]["id"]);
+      $statement->execute();
+      // header('Location: index.php');
+    }
   }
+} catch (PDOException $e) {;
+  $errors[] = "$name already exists in $school_id";
 }
 
 ?>
